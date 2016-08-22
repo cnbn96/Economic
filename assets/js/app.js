e18c495e5495960d68f1b9e86b472e53eb8f31f4
Nguyen Nam Binh <cnbn96@gmail.com>
2016-08-23 01:04:16 +0700

    done slider hot news

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(function(){
+  $('.crsl-items').carousel({
+    visible: 4,
+    itemMinWidth: 250,
+    itemEqualHeight: 400,
+    itemMargin: 9,
+  });
+  
+  $("a[href=#zzz]").on('click', function(e) {
+    e.preventDefault();
+  });
+});
