1326d2ea63827928b2b1c3f40acf33825d1d858c
Nguyen Nam Binh <cnbn96@gmail.com>
2016-08-24 14:26:09 +0700

    done product card

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,57 @@
-﻿$(function(){
-  $('.crsl-items').carousel({
+﻿(function(){
+   $('.hotnews-slider').carousel({
     visible: 4,
     itemMinWidth: 250,
     itemEqualHeight: 400,
-    itemMargin: 9,
+    itemMargin: 10,
   });
-  
-  $("a[href=#hotnews]").on('click', function(e) {
-    e.preventDefault();
+  $('.main-slider').carousel({    
+    visible: 1,
+    itemMargin: 0,
+    autoRotate: 6000
   });
-});
+  $('.audio-brands-slider').carousel({
+    visible: 6,
+    itemMinWidth: 96.92,
+    itemEqualHeight: 65,
+    itemMargin: 0,
+  });
+  $('.audio-product-slider').carousel({    
+    visible: 3,
+    itemMinWidth: 250,
+    itemEqualHeight: 400
+  });
+  $('.speaker-brands-slider').carousel({
+    visible: 6,
+    itemMinWidth: 96.92,
+    itemEqualHeight: 65,
+    itemMargin: 0,
+  });
+  $('.speaker-product-slider').carousel({    
+    visible: 3,
+    itemMinWidth: 250,
+    itemEqualHeight: 400
+  });
+  $('.computer-brands-slider').carousel({
+    visible: 6,
+    itemMinWidth: 96.92,
+    itemEqualHeight: 65,
+    itemMargin: 0,
+  });
+  $('.computer-product-slider').carousel({    
+    visible: 3,
+    itemMinWidth: 250,
+    itemEqualHeight: 400
+  });
+  $('.gear-brands-slider').carousel({
+    visible: 6,
+    itemMinWidth: 96.92,
+    itemEqualHeight: 65,
+    itemMargin: 0,
+  });
+  $('.gear-product-slider').carousel({    
+    visible: 3,
+    itemMinWidth: 250,
+    itemEqualHeight: 400
+  });
+}())
